4083cb1eb26af39abc21e5b36494fe614be43858
Karima-Aktari <karimaaktari911@gmail.com>
2021-09-02 16:03:35 +0600

    first commit

---
+++ js/book.js
@@ -0,0 +1,58 @@
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const showData = document.getElementById('show-data');
+const errorMessage = document.getElementById('error-message');
+const displayLength = document.getElementById('length');
+
+
+searchButton.addEventListener('click', function () {
+    loadData();
+    searchInput.value = "";
+    displayLength.innerText = "";
+});
+
+const loadData = () => {
+    const searchText = searchInput.value;
+    showData.innerHTML = "";
+    if (searchText === "") {
+        errorMessage.innerText = "Search Field Can't Be Empty.";
+        return;
+    }
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showGetData(data.docs))
+}
+
+// Showing data
+const showGetData = (books) => {
+    console.log(books);
+    console.log(displayLength.innerText);
+
+    if (books.length === 0) {
+        errorMessage.innerText = "NO Result Found";
+        displayLength.innerText = `Length: ${books.length}`;
+    } else {
+        errorMessage.innerText = "";
+        displayLength.innerText = `Length: ${books.length}`;
+    }
+
+    const booksData = (books.splice(0, 20));
+    // console.log(booksData);
+    booksData.forEach((book) => {
+        console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card mx-auto rounded-3 h-100 p-1">
+                <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-100 p-4" alt="...">
+                <div class="card-body overflow-hidden">
+                    <h2 class="card-title text-success">Book: ${book.title}</h2>
+                    <h3 class="text-secondary">Author: ${book.author_name ? book.author_name : "Not Available"}</h3>
+                    <h4 class="text-secondary">Publisher: ${book.publisher ? book.publisher : "Not Available"}</h4>
+                    <h4 class="text-secondary">First published: ${book.first_publish_year}</h4>
+                </div>
+            </div>`
+        showData.appendChild(div);
+    })
+}
